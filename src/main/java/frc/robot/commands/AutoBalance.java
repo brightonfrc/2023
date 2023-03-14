779643e537b2864955cabe8d63dad614f9848d4f
WebCoder49 <69071853+WebCoder49@users.noreply.github.com>
2023-03-14 17:22:46 +0000

    Start adding autobalance code

--- src/main/java/frc/robot/commands/AutoBalance.java
+++ src/main/java/frc/robot/commands/AutoBalance.java
@@ -4,44 +4,153 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.ProfiledPIDCommand;
-import frc.robot.Constants.MotionParameters.Autobalance;
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+import edu.wpi.first.wpilibj.ADIS16470_IMU.IMUAxis;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DifferentialDriveSubsystem;
 import frc.robot.subsystems.Gyro;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// information, see:
-// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class AutoBalance extends ProfiledPIDCommand {
-  // Create our gyro object like we would on a real robot.
+/**
+ * A command for driving up the charge station and balancing on top of it. Thanks to https://www.chiefdelphi.com/t/psa-balance-in-auto/ .
+ */
+public class AutoBalance extends CommandBase {
 
-  /** Creates a new AutoBalance command. */
+  /** 
+   * The built-in RoboRIO Accelerometer
+   */
+  private Gyro m_gyro;
+  /** 
+   * The current state of the autonomous routine
+   * For pure balance
+   * > 0 = drive forwards to approach station
+   * > 1 = drive up charge station
+   * > 2 = on charge station - stop
+   * > 3/4 = stopped
+   */
+  private int state;
+  /**
+   * Timer from start of state trigger to state entry
+   */
+  private int debounceCount;
+  /**
+   * Robot speed when driving up slope of charge station
+   */
+  private double robotSpeedSlow;
+  /**
+   * Robot speed when driving towards charge station
+   */
+  private double robotSpeedFast;
+  /**
+   * Minimum Angle at which the robot is driving up the charge station (degrees)
+   */
+  private double onChargeStationDegree;
+  /**
+   * Maximum Angle at which the robot is level, on top of the charge station (degrees)
+   */
+  private double levelDegree;
+  /**
+   * Time delay from start of state trigger to state entry (seconds)
+   */
+  private double debounceTime;
+
   public AutoBalance(Gyro gyro, DifferentialDriveSubsystem drivetrain) {
-    super(
-        // The ProfiledPIDController used by the command
-        new ProfiledPIDController(
-            // The PID gains
-            Autobalance.k_p,
-            Autobalance.k_i,
-            Autobalance.k_d,
-            // The motion profile constraints
-            new TrapezoidProfile.Constraints(Autobalance.k_maxVelocity, Autobalance.k_maxAcceleration)),
-        // This should return the measurement
-        () -> gyro.gyro.getAngle(),
-        // This should return the goal (can also be a constant)
-        // We need to have an angle of 0 degrees and a velocity of 0
-        () -> new TrapezoidProfile.State(0, 0),
-        // This uses the output
-        (output, setpoint) -> {
-          SmartDashboard.putNumber("Autobalance.output", output);
-          // Use the output (and setpoint, if desired) here
-        });
-    // Use addRequirements() here to declare subsystem dependencies.
-    // Configure additional PID options by calling `getController` here.
     addRequirements(drivetrain, gyro);
+
+    m_gyro = gyro;
+    state = 0;
+    debounceCount = 0;
+
+    /**********
+     * CONFIG *
+     **********/
+    // Speed the robot drived while scoring/approaching station, default = 0.4
+    robotSpeedFast = 0.4;
+
+    // Speed the robot drives while balancing itself on the charge station.
+    // Should be roughly half the fast speed, to make the robot more accurate,
+    // default = 0.2
+    robotSpeedSlow = 0.2;
+
+    // Angle where the robot knows it is on the charge station, default = 13.0
+    onChargeStationDegree = 13.0;
+
+    // Angle where the robot can assume it is level on the charging station
+    // Used for exiting the drive forward sequence as well as for auto balancing,
+    // default = 6.0
+    levelDegree = 6.0;
+
+    // Amount of time a sensor condition needs to be met before changing states in
+    // seconds
+    // Reduces the impact of sensor noice, but too high can make the auto run
+    // slower, default = 0.2
+    debounceTime = 0.2;
+    // TODO: Construct command
+  }
+
+
+  // returns the magnititude of the robot's tilt calculated by Pythagoras of pitch
+  // and roll, used to compensate for diagonally mounted rio
+  public double getTilt() {
+    // TODO: Check
+    double pitch = m_gyro.getAngle(IMUAxis.kX).getDegrees();
+    double roll = m_gyro.getAngle(IMUAxis.kY).getDegrees();
+    if ((pitch + roll) >= 0) {
+        return Math.sqrt(pitch * pitch + roll * roll);
+    } else {
+        return -Math.sqrt(pitch * pitch + roll * roll);
+    }
+  }
+
+  public int secondsToTicks(double time) {
+    return (int) (time * 50);
+  }
+
+  // routine for automatically driving onto and engaging the charge station.
+  // returns a value from -1.0 to 1.0, which left and right motors should be set
+  // to.
+  public double autoBalanceRoutine() {
+    switch (state) {
+        // drive forwards to approach station, exit when tilt is detected
+        case 0:
+            if (getTilt() > onChargeStationDegree) {
+                debounceCount++;
+            }
+            if (debounceCount > secondsToTicks(debounceTime)) {
+                state = 1;
+                debounceCount = 0;
+                return robotSpeedSlow;
+            }
+            return robotSpeedFast;
+        // driving up charge station, drive slower, stopping when level
+        case 1:
+            if (getTilt() < levelDegree) {
+                debounceCount++;
+            }
+            if (debounceCount > secondsToTicks(debounceTime)) {
+                state = 2;
+                debounceCount = 0;
+                return 0;
+            }
+            return robotSpeedSlow;
+        // on charge station, stop motors and wait for end of auto
+        case 2:
+            if (Math.abs(getTilt()) <= levelDegree / 2) {
+                debounceCount++;
+            }
+            if (debounceCount > secondsToTicks(debounceTime)) {
+                state = 4;
+                debounceCount = 0;
+                return 0;
+            }
+            if (getTilt() >= levelDegree) {
+                return 0.1;
+            } else if (getTilt() <= -levelDegree) {
+                return -0.1;
+            }
+        case 3:
+            return 0;
+    }
+    return 0;
   }
 
   // Returns true when the command should end.
