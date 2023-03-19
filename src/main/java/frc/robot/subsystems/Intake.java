08c4da299778b9c54d49d3b8146a93168fe8c874
WebCoder49 <69071853+WebCoder49@users.noreply.github.com>
2023-03-19 09:29:15 +0000

    Start adding debugging intake subsystem

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+    private TalonSRX m_motor;
+
+    @Override
+    public void periodic() {
+        m_motor.set(ControlMode.Velocity, 0.5);
+        
+        // Output motor current
+        SmartDashboard.putNumber("Intake/InputCurrent", m_motor.getSupplyCurrent());
+        SmartDashboard.putNumber("Intake/OutputCurrent", m_motor.getStatorCurrent());
+    }
+}
